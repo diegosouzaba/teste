62d4c3adba1545576a716f2d7c1fb41d6d1e01a6
diegosouzaba <diego.souzaba@hotmail.com>
2019-01-31 15:57:57 -0300

    Continuação

---
+++ polimorfismo/src/com/br/polimorfismo/Animal.java
@@ -0,0 +1,13 @@
+package com.br.polimorfismo;
+
+public abstract class  Animal {
+	
+	private double peso;
+	private int idade;
+	private int membro;
+	
+	public  abstract void locomover();
+	public  abstract void alimentar();
+	public  abstract void emitirsom();
+
+}
---
+++ polimorfismo/src/com/br/polimorfismo/Reptil.java
@@ -0,0 +1,23 @@
+package com.br.polimorfismo;
+
+public class Reptil  extends Animal{
+
+	@Override
+	public void locomover() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alimentar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emitirsom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
